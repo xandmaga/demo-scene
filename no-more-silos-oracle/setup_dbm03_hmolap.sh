628a4923d6a14d118d96a457a7f1950634a01fe0
Alexandre Gomes <xandmaga@outlook.com>
2021-08-03 09:43:49 -0300

    configuracoes para o logminer

--- no-more-silos-oracle/setup_dbm03_hmolap.sh
+++ no-more-silos-oracle/setup_dbm03_hmolap.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-echo -e $(date) "Firing up Oracle"
-docker-compose up -d oracle
-
-# ---- Wait for Oracle DB to be up (takes several minutes to instantiate) ---
-echo -e "\n--\n\n$(date) Waiting for Oracle to be available … ⏳"
-grep -q "DATABASE IS READY TO USE!" <(docker logs -f oracle)
-echo -e "$(date) Installing rlwrap on Oracle container"
-docker exec --interactive --tty --user root --workdir / $(docker ps --filter "name=oracle" --quiet) bash -c 'rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm'
-docker exec --interactive --tty --user root --workdir / $(docker ps --filter "name=oracle" --quiet) bash -c 'yum install -y rlwrap'
-
 echo -e $(date) "Firing up the rest of the stack"
 docker-compose up -d
 
@@ -29,16 +19,15 @@ curl -i -X POST -H "Accept:application/json" \
           "config": {
             "connector.class": "io.debezium.connector.oracle.OracleConnector",
             "tasks.max" : "1",
-            "database.server.name" : "7cfb0dfca363",
-            "database.url" : "jdbc:oracle:thin:@172.19.0.2:1521/ORCLCDB",
-            "database.user" : "c##dbzuser",
-            "database.password" : "dbz",
-            "database.dbname" : "ORCLCDB",
-            "database.pdb.name" : "ORCLPDB1",
+            "database.server.name" : "HMLOLAP2",
+            "database.url" : "jdbc:oracle:thin:@10.0.182.232:1521/DBM03.HMLOLAP",
+            "database.dbname" : "HMLOLAP",
+            "database.pdb.name" : "DBM03",
             "database.history.kafka.bootstrap.servers" : "kafka:29092",
             "database.history.kafka.topic": "schema-changes-inventory",
             "include.schema.changes": "true",
             "database.connection.adapter": "logminer",
+            "table.include.list": "SJD_ODS.PE_TB_PROCESSO_TRF",
             "log.mining.strategy": "online_catalog",
             "key.converter": "io.confluent.connect.avro.AvroConverter",
             "key.converter.schema.registry.url": "http://schema-registry:8081",
